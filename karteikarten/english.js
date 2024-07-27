a6b713138da18c312fe50bb5bd7282ddb935c420
antuntune <antekljaj@gmail.com>
2024-07-27 17:13:24 +0200

    fasf

--- karteikarten/english.js
+++ karteikarten/english.js
@@ -1,41 +1,502 @@
 export const cards = [
   {
     id: 1,
-    frontHTML: (
-      <div>
-        What is the capital of <u>Alaska</u>?
-      </div>
-    ),
-    backHTML: <>Juneau</>,
+    frontHTML: <>What is the German word for Application?</>,
+    backHTML: <>Anwendung</>,
   },
   {
     id: 2,
-    frontHTML: <>What is the capital of California?</>,
-    backHTML: <>Sacramento</>,
+    frontHTML: <>What is the German word for Backup?</>,
+    backHTML: <>Sicherung</>,
   },
   {
     id: 3,
-    frontHTML: <>What is the capital of New York?</>,
-    backHTML: <>Albany</>,
+    frontHTML: <>What is the German word for Browser?</>,
+    backHTML: <>Browser</>,
   },
   {
     id: 4,
-    frontHTML: <>What is the capital of Florida?</>,
-    backHTML: <>Tallahassee</>,
+    frontHTML: <>What is the German word for Cable?</>,
+    backHTML: <>Kabel</>,
   },
   {
     id: 5,
-    frontHTML: <>What is the capital of Texas?</>,
-    backHTML: <>Austin</>,
+    frontHTML: <>What is the German word for Cloud?</>,
+    backHTML: <>Wolke</>,
   },
   {
     id: 6,
-    frontHTML: <>What is the capital of New Mexico?</>,
-    backHTML: <>Santa Fe</>,
+    frontHTML: <>What is the German word for Command?</>,
+    backHTML: <>Befehl</>,
   },
   {
     id: 7,
-    frontHTML: <>What is the capital of Arizona?</>,
-    backHTML: <>Phoenix</>,
+    frontHTML: <>What is the German word for Configuration?</>,
+    backHTML: <>Konfiguration</>,
+  },
+  {
+    id: 8,
+    frontHTML: <>What is the German word for Data?</>,
+    backHTML: <>Daten</>,
+  },
+  {
+    id: 9,
+    frontHTML: <>What is the German word for Debug?</>,
+    backHTML: <>Fehlersuche</>,
+  },
+  {
+    id: 10,
+    frontHTML: <>What is the German word for Desktop?</>,
+    backHTML: <>Schreibtisch</>,
+  },
+  {
+    id: 11,
+    frontHTML: <>What is the German word for Device?</>,
+    backHTML: <>Gerät</>,
+  },
+  {
+    id: 12,
+    frontHTML: <>What is the German word for Disk?</>,
+    backHTML: <>Festplatte</>,
+  },
+  {
+    id: 13,
+    frontHTML: <>What is the German word for Download?</>,
+    backHTML: <>Herunterladen</>,
+  },
+  {
+    id: 14,
+    frontHTML: <>What is the German word for Encryption?</>,
+    backHTML: <>Verschlüsselung</>,
+  },
+  {
+    id: 15,
+    frontHTML: <>What is the German word for Firewall?</>,
+    backHTML: <>Firewall</>,
+  },
+  {
+    id: 16,
+    frontHTML: <>What is the German word for Folder?</>,
+    backHTML: <>Ordner</>,
+  },
+  {
+    id: 17,
+    frontHTML: <>What is the German word for Framework?</>,
+    backHTML: <>Rahmenwerk</>,
+  },
+  {
+    id: 18,
+    frontHTML: <>What is the German word for Gateway?</>,
+    backHTML: <>Gateway</>,
+  },
+  {
+    id: 19,
+    frontHTML: <>What is the German word for Hard Drive?</>,
+    backHTML: <>Festplatte</>,
+  },
+  {
+    id: 20,
+    frontHTML: <>What is the German word for Hardware?</>,
+    backHTML: <>Hardware</>,
+  },
+  {
+    id: 21,
+    frontHTML: <>What is the German word for Host?</>,
+    backHTML: <>Host</>,
+  },
+  {
+    id: 22,
+    frontHTML: <>What is the German word for Interface?</>,
+    backHTML: <>Schnittstelle</>,
+  },
+  {
+    id: 23,
+    frontHTML: <>What is the German word for Keyboard?</>,
+    backHTML: <>Tastatur</>,
+  },
+  {
+    id: 24,
+    frontHTML: <>What is the German word for Key?</>,
+    backHTML: <>Schlüssel</>,
+  },
+  {
+    id: 25,
+    frontHTML: <>What is the German word for Laptop?</>,
+    backHTML: <>Laptop</>,
+  },
+  {
+    id: 26,
+    frontHTML: <>What is the German word for Link?</>,
+    backHTML: <>Verbindung</>,
+  },
+  {
+    id: 27,
+    frontHTML: <>What is the German word for Memory?</>,
+    backHTML: <>Speicher</>,
+  },
+  {
+    id: 28,
+    frontHTML: <>What is the German word for Monitor?</>,
+    backHTML: <>Bildschirm</>,
+  },
+  {
+    id: 29,
+    frontHTML: <>What is the German word for Network?</>,
+    backHTML: <>Netzwerk</>,
+  },
+  {
+    id: 30,
+    frontHTML: <>What is the German word for Node?</>,
+    backHTML: <>Knoten</>,
+  },
+  {
+    id: 31,
+    frontHTML: <>What is the German word for Operating System?</>,
+    backHTML: <>Betriebssystem</>,
+  },
+  {
+    id: 32,
+    frontHTML: <>What is the German word for Password?</>,
+    backHTML: <>Passwort</>,
+  },
+  {
+    id: 33,
+    frontHTML: <>What is the German word for Processor?</>,
+    backHTML: <>Prozessor</>,
+  },
+  {
+    id: 34,
+    frontHTML: <>What is the German word for Protocol?</>,
+    backHTML: <>Protokoll</>,
+  },
+  {
+    id: 35,
+    frontHTML: <>What is the German word for Queue?</>,
+    backHTML: <>Warteschlange</>,
+  },
+  {
+    id: 36,
+    frontHTML: <>What is the German word for Router?</>,
+    backHTML: <>Router</>,
+  },
+  {
+    id: 37,
+    frontHTML: <>What is the German word for Security?</>,
+    backHTML: <>Sicherheit</>,
+  },
+  {
+    id: 38,
+    frontHTML: <>What is the German word for Server?</>,
+    backHTML: <>Server</>,
+  },
+  {
+    id: 39,
+    frontHTML: <>What is the German word for Software?</>,
+    backHTML: <>Software</>,
+  },
+  {
+    id: 40,
+    frontHTML: <>What is the German word for Source Code?</>,
+    backHTML: <>Quellcode</>,
+  },
+  {
+    id: 41,
+    frontHTML: <>What is the German word for Storage?</>,
+    backHTML: <>Speicher</>,
+  },
+  {
+    id: 42,
+    frontHTML: <>What is the German word for Switch?</>,
+    backHTML: <>Schalter</>,
+  },
+  {
+    id: 43,
+    frontHTML: <>What is the German word for Terminal?</>,
+    backHTML: <>Terminal</>,
+  },
+  {
+    id: 44,
+    frontHTML: <>What is the German word for Transfer?</>,
+    backHTML: <>Übertragung</>,
+  },
+  {
+    id: 45,
+    frontHTML: <>What is the German word for Update?</>,
+    backHTML: <>Aktualisierung</>,
+  },
+  {
+    id: 46,
+    frontHTML: <>What is the German word for Upgrade?</>,
+    backHTML: <>Aufrüstung</>,
+  },
+  {
+    id: 47,
+    frontHTML: <>What is the German word for Upload?</>,
+    backHTML: <>Hochladen</>,
+  },
+  {
+    id: 48,
+    frontHTML: <>What is the German word for User?</>,
+    backHTML: <>Benutzer</>,
+  },
+  {
+    id: 49,
+    frontHTML: <>What is the German word for Username?</>,
+    backHTML: <>Benutzername</>,
+  },
+  {
+    id: 50,
+    frontHTML: <>What is the German word for Utility?</>,
+    backHTML: <>Dienstprogramm</>,
+  },
+  {
+    id: 51,
+    frontHTML: <>What is the German word for Virtual?</>,
+    backHTML: <>Virtuell</>,
+  },
+  {
+    id: 52,
+    frontHTML: <>What is the German word for Virus?</>,
+    backHTML: <>Virus</>,
+  },
+  {
+    id: 53,
+    frontHTML: <>What is the German word for Web?</>,
+    backHTML: <>Web</>,
+  },
+  {
+    id: 54,
+    frontHTML: <>What is the German word for Website?</>,
+    backHTML: <>Webseite</>,
+  },
+  {
+    id: 55,
+    frontHTML: <>What is the German word for Wireless?</>,
+    backHTML: <>Drahtlos</>,
+  },
+  {
+    id: 56,
+    frontHTML: <>What is the German word for Algorithm?</>,
+    backHTML: <>Algorithmus</>,
+  },
+  {
+    id: 57,
+    frontHTML: <>What is the German word for Bandwidth?</>,
+    backHTML: <>Bandbreite</>,
+  },
+  {
+    id: 58,
+    frontHTML: <>What is the German word for Bug?</>,
+    backHTML: <>Fehler</>,
+  },
+  {
+    id: 59,
+    frontHTML: <>What is the German word for Cache?</>,
+    backHTML: <>Zwischenspeicher</>,
+  },
+  {
+    id: 60,
+    frontHTML: <>What is the German word for Cipher?</>,
+    backHTML: <>Chiffre</>,
+  },
+  {
+    id: 61,
+    frontHTML: <>What is the German word for Client?</>,
+    backHTML: <>Client</>,
+  },
+  {
+    id: 62,
+    frontHTML: <>What is the German word for Compiler?</>,
+    backHTML: <>Compiler</>,
+  },
+  {
+    id: 63,
+    frontHTML: <>What is the German word for Cookie?</>,
+    backHTML: <>Cookie</>,
+  },
+  {
+    id: 64,
+    frontHTML: <>What is the German word for Crash?</>,
+    backHTML: <>Absturz</>,
+  },
+  {
+    id: 65,
+    frontHTML: <>What is the German word for Cyber?</>,
+    backHTML: <>Cyber</>,
+  },
+  {
+    id: 66,
+    frontHTML: <>What is the German word for Dashboard?</>,
+    backHTML: <>Armaturenbrett</>,
+  },
+  {
+    id: 67,
+    frontHTML: <>What is the German word for Digital?</>,
+    backHTML: <>Digital</>,
+  },
+  {
+    id: 68,
+    frontHTML: <>What is the German word for Domain?</>,
+    backHTML: <>Domain</>,
+  },
+  {
+    id: 69,
+    frontHTML: <>What is the German word for Emulation?</>,
+    backHTML: <>Emulation</>,
+  },
+  {
+    id: 70,
+    frontHTML: <>What is the German word for Firmware?</>,
+    backHTML: <>Firmware</>,
+  },
+  {
+    id: 71,
+    frontHTML: <>What is the German word for Graphics?</>,
+    backHTML: <>Grafik</>,
+  },
+  {
+    id: 72,
+    frontHTML: <>What is the German word for Hyperlink?</>,
+    backHTML: <>Hyperlink</>,
+  },
+  {
+    id: 73,
+    frontHTML: <>What is the German word for Internet?</>,
+    backHTML: <>Internet</>,
+  },
+  {
+    id: 74,
+    frontHTML: <>What is the German word for Log?</>,
+    backHTML: <>Protokoll</>,
+  },
+  {
+    id: 75,
+    frontHTML: <>What is the German word for Malware?</>,
+    backHTML: <>Schadsoftware</>,
+  },
+  {
+    id: 76,
+    frontHTML: <>What is the German word for Middleware?</>,
+    backHTML: <>Middleware</>,
+  },
+  {
+    id: 77,
+    frontHTML: <>What is the German word for Modem?</>,
+    backHTML: <>Modem</>,
+  },
+  {
+    id: 78,
+    frontHTML: <>What is the German word for Module?</>,
+    backHTML: <>Modul</>,
+  },
+  {
+    id: 79,
+    frontHTML: <>What is the German word for Network?</>,
+    backHTML: <>Netzwerk</>,
+  },
+  {
+    id: 80,
+    frontHTML: <>What is the German word for Packet?</>,
+    backHTML: <>Paket</>,
+  },
+  {
+    id: 81,
+    frontHTML: <>What is the German word for Patch?</>,
+    backHTML: <>Patch</>,
+  },
+  {
+    id: 82,
+    frontHTML: <>What is the German word for Pixel?</>,
+    backHTML: <>Pixel</>,
+  },
+  {
+    id: 83,
+    frontHTML: <>What is the German word for Plugin?</>,
+    backHTML: <>Plugin</>,
+  },
+  {
+    id: 84,
+    frontHTML: <>What is the German word for Port?</>,
+    backHTML: <>Port</>,
+  },
+  {
+    id: 85,
+    frontHTML: <>What is the German word for Proxy?</>,
+    backHTML: <>Proxy</>,
+  },
+  {
+    id: 86,
+    frontHTML: <>What is the German word for Runtime?</>,
+    backHTML: <>Laufzeit</>,
+  },
+  {
+    id: 87,
+    frontHTML: <>What is the German word for Sandbox?</>,
+    backHTML: <>Sandbox</>,
+  },
+  {
+    id: 88,
+    frontHTML: <>What is the German word for Script?</>,
+    backHTML: <>Skript</>,
+  },
+  {
+    id: 89,
+    frontHTML: <>What is the German word for Search Engine?</>,
+    backHTML: <>Suchmaschine</>,
+  },
+  {
+    id: 90,
+    frontHTML: <>What is the German word for Session?</>,
+    backHTML: <>Sitzung</>,
+  },
+  {
+    id: 91,
+    frontHTML: <>What is the German word for Shell?</>,
+    backHTML: <>Shell</>,
+  },
+  {
+    id: 92,
+    frontHTML: <>What is the German word for Spreadsheet?</>,
+    backHTML: <>Tabellenkalkulation</>,
+  },
+  {
+    id: 93,
+    frontHTML: <>What is the German word for Subnet?</>,
+    backHTML: <>Subnetz</>,
+  },
+  {
+    id: 94,
+    frontHTML: <>What is the German word for Syntax?</>,
+    backHTML: <>Syntax</>,
+  },
+  {
+    id: 95,
+    frontHTML: <>What is the German word for Terminal?</>,
+    backHTML: <>Terminal</>,
+  },
+  {
+    id: 96,
+    frontHTML: <>What is the German word for Traffic?</>,
+    backHTML: <>Verkehr</>,
+  },
+  {
+    id: 97,
+    frontHTML: <>What is the German word for Trojan?</>,
+    backHTML: <>Trojaner</>,
+  },
+  {
+    id: 98,
+    frontHTML: <>What is the German word for Upload?</>,
+    backHTML: <>Hochladen</>,
+  },
+  {
+    id: 99,
+    frontHTML: <>What is the German word for URL?</>,
+    backHTML: <>URL</>,
+  },
+  {
+    id: 100,
+    frontHTML: <>What is the German word for Utility?</>,
+    backHTML: <>Dienstprogramm</>,
   },
 ];
---
+++ karteikarten/style.ts
@@ -0,0 +1,27 @@
+export const flashStyleContentFront: React.CSSProperties = {
+  fontSize: "25px", // Correct type
+  fontWeight: "bold",
+  textAlign: "center",
+  verticalAlign: "middle",
+  flex: "0 0 120px",
+};
+
+export const flashStyleContentBack: React.CSSProperties = {
+  fontSize: "25px", // Correct type
+  fontWeight: "bold",
+  flex: "0 0 120px",
+};
+
+export const flashStyleCardFront: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+};
+
+export const flashStyleCardBack: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+};
