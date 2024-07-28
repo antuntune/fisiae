d9f72895c431e7b0071a6e387fd13205c05e6d7f
antuntune <antekljaj@gmail.com>
2024-07-28 11:20:58 +0200

    fasfdas

---
+++ components/comments.jsx
@@ -0,0 +1,20 @@
+import Giscus from "@giscus/react";
+export default function Comments() {
+  return (
+    <Giscus
+      id="comments"
+      repo="antuntune/fisiae"
+      repoId="R_kgDOMZtxMg"
+      category="General"
+      categoryId="DIC_kwDOMZtxMs4ChO3B"
+      mapping="pathname"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="preferred_color_scheme"
+      lang="en"
+      loading="lazy"
+    />
+  );
+}
