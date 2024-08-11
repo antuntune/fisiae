b5767cb9b909d68e172e7c96921c0229ec7c3815
antuntune <antekljaj@gmail.com>
2024-08-11 21:27:26 +0200

    fdsa

--- components/videoPlayer.jsx
+++ components/videoPlayer.jsx
@@ -2,9 +2,9 @@ export default function VideoPlayer() {
   return (
     <iframe
       style={{ borderRadius: "1.5em" }}
-      width="350"
+      width="354"
       height="200"
-      src="https://www.youtube.com/embed/oN6doVEr1XM?si=b8lAK1rA3TxaOjsg&amp;start=17"
+      src="https://www.youtube.com/embed/PcR8I-7oCnU?si=RfcnO4tBCU-1zaNH"
       title="YouTube video player"
       frameborder="8"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
